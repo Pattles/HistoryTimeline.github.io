0624b80a80293fe5a9f5af34b6c22f0bae3e5e02
Pattles <doomrock8@gmail.com>
2026-03-15 20:45:18 -0400

    split js into functions

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Declaring important stuff
+let allEvents = []
+
 // Formatting dates into readable ones
 function formatDate(dateStr) {
     const date = new Date(dateStr + 'T00:00:00');
@@ -12,74 +15,30 @@ function formatDate(dateStr) {
     return `${month} ${day}${suffix} ${year}`;
 };
 
-// On page load
-let allEvents = []
-
-document.addEventListener('DOMContentLoaded', () => {
-// all your code here
-    fetch('/get-events')
-        .then(res => res.json())
-        .then(events => {
-            allEvents = events;
-        });
-
-    document.getElementById('section-events__headline').innerHTML = `What happened on ${formatDate(new Date().toISOString().split('T')[0])}?`;
-    
-    // # Adding a new card on button press
-    const heroBtnGen = document.getElementById('hero-btnGen');
+// # Adding a new card on button press
+function handleGenerate() {
+    // Clearing everything first under the div
+    document.getElementById('section-events__cards').innerHTML = '';
 
     const cardsContainer = document.getElementById('section-events__cards');
-    heroBtnGen.addEventListener('click', () => {
-        // Clearing everything first under the div
-        document.getElementById('section-events__cards').innerHTML = '';
 
+    // Getting all relevant events
+    const heroDate = document.getElementById('hero-date').value;
+    const results = allEvents.filter(event => event.date === heroDate);
 
-        // Getting all relevant events
-        const heroDate = document.getElementById('hero-date').value;
-        const results = allEvents.filter(event => event.date === heroDate);
+    // Changing headline -> What happened on [date]? – if no date/invalid date entered, default to today's date
+    document.getElementById('section-events__headline').innerHTML = `What happened on ${formatDate(heroDate || new Date().toISOString().split('T')[0])}?`;
 
-        // Changing headline -> What happened on [date]? – if no date/invalid date entered, default to today's date
-        document.getElementById('section-events__headline').innerHTML = `What happened on ${formatDate(heroDate || new Date().toISOString().split('T')[0])}?`;
-
-        if (results.length === 0 || !heroDate) {
-            // Create a bogus card
-            const card = document.createElement('div');
-            card.classList.add('section-events__card');
-            card.innerHTML = `
-                <h4>Nothing happened!</h4>
-                <p>Just kidding, I didn\'t add anything for this date</p>
-            `;
-            cardsContainer.appendChild(card); 
-        } else {
-            // Creating a new card for each event
-            results.forEach(event => {
-                const card = document.createElement('div');
-                card.classList.add('section-events__card');
-                card.innerHTML = `
-                    <h4>${event.title}${event.link ? ` - <a href="${event.link}" target="_blank">Read more</a>` : ''}</h4>
-                    <p>${event.desc}</p>
-                `;
-                cardsContainer.appendChild(card);
-            });
-        }
-    });
-
-    // Generating random date
-    const heroBtnRandom = document.getElementById('hero-btnRandom');
-
-    heroBtnRandom.addEventListener('click', () => {
-        // Clearing everything first under the div
-        document.getElementById('section-events__cards').innerHTML = '';
-
-        // Getting a random date
-        const allDates = allEvents.map(event => event.date);
-        const randomDate = allDates[Math.floor(Math.random() * allDates.length)];
-
-        // Changing headline -> What happened on [date]? – if no date/invalid date entered, default to today's date
-        document.getElementById('section-events__headline').innerHTML = `What happened on ${formatDate(randomDate)}?`;
-
-        const results = allEvents.filter(event => event.date === randomDate);
-
+    if (results.length === 0 || !heroDate) {
+        // Create a bogus card
+        const card = document.createElement('div');
+        card.classList.add('section-events__card');
+        card.innerHTML = `
+            <h4>Nothing happened!</h4>
+            <p>Just kidding, I didn\'t add anything for this date</p>
+        `;
+        cardsContainer.appendChild(card); 
+    } else {
         // Creating a new card for each event
         results.forEach(event => {
             const card = document.createElement('div');
@@ -89,44 +48,92 @@ document.addEventListener('DOMContentLoaded', () => {
                 <p>${event.desc}</p>
             `;
             cardsContainer.appendChild(card);
-        });
+        })
+    }
+};
 
+// # Generating random date
+function handleRandom() {
+    // Clearing everything first under the div
+    document.getElementById('section-events__cards').innerHTML = '';
+
+    const cardsContainer = document.getElementById('section-events__cards');
+
+    // Getting a random date
+    const allDates = allEvents.map(event => event.date);
+    const randomDate = allDates[Math.floor(Math.random() * allDates.length)];
+
+    // Changing headline -> What happened on [date]? – if no date/invalid date entered, default to today's date
+    document.getElementById('section-events__headline').innerHTML = `What happened on ${formatDate(randomDate)}?`;
+
+    const results = allEvents.filter(event => event.date === randomDate);
+
+    // Creating a new card for each event
+    results.forEach(event => {
+        const card = document.createElement('div');
+        card.classList.add('section-events__card');
+        card.innerHTML = `
+            <h4>${event.title}${event.link ? ` - <a href="${event.link}" target="_blank">Read more</a>` : ''}</h4>
+            <p>${event.desc}</p>
+        `;
+        cardsContainer.appendChild(card);
     });
 
-    // # Making a popup
+}
+
+// Handling opening the Addnew modal
+function handleAddnewOpen() {
     const modalAddnew = document.getElementById('modal-addnew')
-    const heroBtnAddnew = document.getElementById('hero-btnAddnew');
 
     // Making the popup visible on click
-    heroBtnAddnew.addEventListener('click', () => {
-        modalAddnew.style.display = "flex";
+    modalAddnew.style.display = "flex";
+};
+
+// Handling closing the Addnew modal
+function handleAddnewClose() {
+    const modalAddnew = document.getElementById('modal-addnew')
+
+    // Making the popup invisible on click
+    modalAddnew.style.display = "none";
+};
+
+// Handling submitting content from Addnew modal with content
+function handleAddnewSubmit() {
+    // Getting event date & time
+    const date = document.getElementById('input-addnewDate').value
+
+    // Getting event content
+    const title = document.getElementById('input-addnewTitle').value;
+    const desc = document.getElementById('input-addnewDesc').value;
+    const link = document.getElementById('input-addnewLink').value;
+
+    fetch('/add-event', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({date, title, desc, link})
+    }).then(() => {
+        location.reload();
     });
+};
 
-    // Storing the content from popup
-    const modalBtnSubmit = document.getElementById('modal-addnewSubmit');
-
-    modalBtnSubmit.addEventListener('click', () => {
-        // Getting event date & time
-        const date = document.getElementById('input-addnewDate').value
-
-        // Getting event content
-        const title = document.getElementById('input-addnewTitle').value;
-        const desc = document.getElementById('input-addnewDesc').value;
-        const link = document.getElementById('input-addnewLink').value;
-
-        fetch('/add-event', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({date, title, desc, link})
-        }).then(() => {
-            location.reload();
+// On page load
+document.addEventListener('DOMContentLoaded', () => {
+    // Fetching events.json
+    fetch('/get-events')
+        .then(res => res.json())
+        .then(events => {
+            allEvents = events;
         });
-    }); 
 
-    // Closing the popup
-    const modalBtnClose = document.getElementById('modal-addnewClose')
+    // Changing the headline to today's date
+    document.getElementById('section-events__headline').innerHTML = `What happened on ${formatDate(new Date().toISOString().split('T')[0])}?`;
 
-    modalBtnClose.addEventListener('click', () => {
-        modalAddnew.style.display = "none";
-    });
+    // Hero buttons
+    document.getElementById('hero-btnGen').addEventListener('click', handleGenerate);
+    document.getElementById('hero-btnRandom').addEventListener('click', handleRandom);
+
+    // Addnew modal
+    document.getElementById('hero-btnAddnew').addEventListener('click', handleAddnewOpen);
+    document.getElementById('modal-addnewClose').addEventListener('click', handleAddnewClose);
+    document.getElementById('modal-addnewSubmit').addEventListener('click', handleAddnewSubmit);
 });
